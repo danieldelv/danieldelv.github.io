9bf2161c6551dba8b3a59cc7202c944eefb1b490
danieldelv <danieldelv@yahoo.com>
2018-04-24 19:07:41 -0400

    homework

--- homework/accordion/js/app.js
+++ homework/accordion/js/app.js
@@ -16,3 +16,39 @@
 // As a user
 // When I click on an opened row
 // I expect the text to disappear more slowly than how it appeared
+
+
+$(".row").click(toggle)
+
+function toggle(event) {
+	var wrapper = $(event.currentTarget).find(".wrapper");
+	wrapper.toggle();
+}
+
+
+//$(".wrapper").find("p").show("p");
+
+
+// $("ul li .social-link")
+
+//⇒ Select elements with class social-link that are inside <li> elements which are inside <ul>
+// $(".wrapper").toggle();
+// $(".wrapper").find("p").show("p")
+
+//$(".rowz").click(reveal);
+
+//function reveal(event) {
+//	var showrow = $(event.currentTarget).show("#row");
+
+//	$(".wrapper").children(reveal)
+
+
+
+
+
+
+// 
+// $("#wrapper").children()
+// $("#hide").click(toggleCody)
+// $("#show").click(toggleCody)
+//$("p").toggle();
